18726c919b0044cba6a5643792bcefb7aa4cf246
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-03-25 23:42:48 +0200

    Added nanosleep and sleep

--- mini-libc/libc/process/sleep.c
+++ mini-libc/libc/process/sleep.c
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
-#include <internal/syscall.h>
 #include <unistd.h>
+#include <time.h>
+#include <internal/syscall.h>
+#include <internal/types.h>
 
 unsigned int sleep(unsigned int seconds)
 {
-	
+	struct timespec ts_req = {seconds, 0};
+	struct timespec ts_rem;
+
+	return nanosleep(&ts_req, &ts_rem) == 0 ? 0 : ts_rem.tv_sec;
 }
