5ca3f5e5dd64e47f7930e1c96ab0a59e966ca4c8
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-03-26 02:08:08 +0200

    Added truncate and ftruncate functions

--- mini-libc/libc/io/ftruncate.c
+++ mini-libc/libc/io/ftruncate.c
@@ -2,10 +2,18 @@
 
 #include <unistd.h>
 #include <internal/syscall.h>
+#include <internal/types.h>
 #include <errno.h>
 
 int ftruncate(int fd, off_t length)
 {
-	/* TODO: Implement ftruncate(). */
-	return -1;
+	// 0 if success, -error otherwise
+	int outcome = syscall(__NR_ftruncate, fd, length);
+
+	if (outcome < 0) {
+		errno = -outcome;
+		return -1;
+	}
+
+	return 0;
 }
--- mini-libc/libc/io/truncate.c
+++ mini-libc/libc/io/truncate.c
@@ -2,10 +2,18 @@
 
 #include <unistd.h>
 #include <internal/syscall.h>
+#include <internal/types.h>
 #include <errno.h>
 
 int truncate(const char *path, off_t length)
 {
-	/* TODO: Implement truncate(). */
-	return -1;
+	// 0 if success, -error otherwise
+	int outcome = syscall(__NR_truncate, path, length);
+
+	if (outcome < 0) {
+		errno = -outcome;
+		return -1;
+	}
+
+	return 0;
 }
