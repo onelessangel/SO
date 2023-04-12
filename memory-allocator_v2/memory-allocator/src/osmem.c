862bd8c4ef3a405375cc8b8c32126e4e7b38c245
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-04-12 11:53:44 +0300

    Correct malloc ad wonky calloc

--- memory-allocator_v2/memory-allocator/src/osmem.c
+++ memory-allocator_v2/memory-allocator/src/osmem.c
@@ -32,29 +32,71 @@ void *os_malloc(size_t size)
 		// facem request_space de la ultimul block free
 		if (global_base == NULL) {
 			printf("uga-buga\n");
-			global_base = request_space(NULL, MMAP_THRESHOLD - METADATA_SIZE);
+			global_base = request_space(&global_base, NULL, MMAP_THRESHOLD - METADATA_SIZE);
 
-			printf("hellllllllllloooooooooo\n");
+			// printf("hellllllllllloooooooooo\n");
 
-			split_block(global_base, aligned_size);			
+			split_block(global_base, aligned_size);
 			global_base->status = STATUS_ALLOC;
 
-			printf("hellllllllllloooooooooo\n");
-			last = global_base;
+			// printf("hellllllllllloooooooooo\n");
+			// last = global_base;
 
 			return (void *)(global_base + 1);
 		}
 
-		// printf("am trecut de primul if\n");
+		printf("am trecut de primul if\n");
 
 		coalesce_blocks(global_base);
 
-		block = get_free_block(global_base, aligned_size);
+		block = get_best_fit(global_base, &last, aligned_size);
 
-		if (block == NULL || block->size < aligned_size) {
-			block = request_space(last, aligned_size);
+		// // || block->size < aligned_size
+		if (block == NULL) {
+			if (last->status == STATUS_FREE) {
+				sbrk(aligned_size - last->size);
+				last->status = STATUS_ALLOC;
+				last->size = aligned_size;
+
+				return (void *)(last + 1);
+			}
+
+			block = request_space(&global_base, last, aligned_size);
+
+			if (block == NULL) {
+				return NULL;
+			}
+			block->status = STATUS_ALLOC;
 			// printf("HUA\n");
+		} else {
+			// size_t leftover_size = aligned_size - last->size;
+			// block = request_space(last, leftover_size);
+			split_block(block, aligned_size);
+			// find_free_block(global_base, &last, aligned_size);
+			// block = last;
 		}
+
+		block->status = STATUS_ALLOC;
+
+		printf("                    \n");
+		return (void *)(block + 1);
+
+
+		// return NULL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		// printf("sunt la capatul puterilor\n");
 		// printf("aligned size: %d \n", aligned_size);
@@ -63,10 +105,10 @@ void *os_malloc(size_t size)
 		// if (block->next) {
 		// 	printf("YUHUUU\n");
 		// }
-		split_block(block, aligned_size);
-		block->status = STATUS_ALLOC;
+		// split_block(block, aligned_size);
+		// block->status = STATUS_ALLOC;
 
-		last = block;	
+		// last = block;	
 
 		// printf("size: %d\n", block->size);
 
@@ -95,8 +137,7 @@ void *os_malloc(size_t size)
 		
 
 		// DIE??
-		printf("                    \n");
-		return (void *)(block + 1);
+		
 	}
 
 	printf("hello\n");
@@ -153,8 +194,94 @@ void os_free(void *ptr)
 
 void *os_calloc(size_t nmemb, size_t size)
 {
-	/* TODO: Implement os_calloc */
-	return NULL;
+	if (size <= 0) {
+		return NULL;
+	}
+
+	size_t aligned_size = ALIGN(size * nmemb);
+	printf("dimensiunea: %d\n", size);
+	printf("dimensiunea aliniata: %d\n", aligned_size);
+
+	int page_size = getpagesize();
+	// static bool heap_is_init = false;
+
+	// if (!heap_is_init) {
+	// 	init_heap(&global_base);
+	// }
+
+	struct block_meta *block;
+
+	if (aligned_size + METADATA_SIZE < page_size) {
+		printf_("am ajuns aici\n");
+		// block = get_free_block(global_base, )
+		// facem request_space de la ultimul block free
+		if (global_base == NULL) {
+			printf("uga-buga\n");
+			global_base = request_space(&global_base, NULL, page_size - METADATA_SIZE);
+
+			// printf("hellllllllllloooooooooo\n");
+
+			split_block(global_base, aligned_size);
+			global_base->status = STATUS_ALLOC;
+
+			// printf("hellllllllllloooooooooo\n");
+			// last = global_base;
+
+			return (void *)(global_base + 1);
+		}
+
+		printf("am trecut de primul if\n");
+
+		coalesce_blocks(global_base);
+
+		block = get_best_fit(global_base, &last, aligned_size);
+
+		// // || block->size < aligned_size
+		if (block == NULL) {
+			if (last->status == STATUS_FREE) {
+				sbrk(aligned_size - last->size);
+				last->status = STATUS_ALLOC;
+				last->size = aligned_size;
+
+				return (void *)(last + 1);
+			}
+
+			block = request_space(&global_base, last, aligned_size);
+
+			if (block == NULL) {
+				return NULL;
+			}
+			block->status = STATUS_ALLOC;
+			// printf("HUA\n");
+		} else {
+			// size_t leftover_size = aligned_size - last->size;
+			// block = request_space(last, leftover_size);
+			split_block(block, aligned_size);
+			// find_free_block(global_base, &last, aligned_size);
+			// block = last;
+		}
+
+		block->status = STATUS_ALLOC;
+
+		printf("                    \n");
+		return (void *)(block + 1);
+
+	}
+
+	block = (struct block_meta *) mmap(NULL, METADATA_SIZE + aligned_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON, -1, 0);
+	block->size = aligned_size;
+	block->status = STATUS_MAPPED;
+	block->next = NULL;
+
+	printf("%d\n", block->size);
+
+	if (global_base == NULL) {
+		global_base = block;
+	}
+
+	// printf("%d", global_base->size);
+
+	return (void *)(block + 1);
 }
 
 void *os_realloc(void *ptr, size_t size)
