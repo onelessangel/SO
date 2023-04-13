10cd635b9935395d73826d65558fb36691491d5a
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-04-13 16:24:28 +0300

    82 points

--- memory-allocator_v2/memory-allocator/src/helpers.c
+++ memory-allocator_v2/memory-allocator/src/helpers.c
@@ -81,7 +81,7 @@ void split_block(struct block_meta *block, size_t size)
 	size_t remaining_size = block->size - aligned_size - METADATA_SIZE;
 
 	if (block->size - aligned_size < METADATA_SIZE + ALIGN(1)) {
-		block->status = STATUS_ALLOC;
+		// block->status = STATUS_ALLOC;
 		return;
 	}
 
@@ -97,13 +97,30 @@ void split_block(struct block_meta *block, size_t size)
 	block->status = STATUS_ALLOC;
 }
 
-void truncate_block(struct block_meta *block, size_t size)
-{
-	size_t aligned_size = ALIGN(size);
-	size_t remaining_size = block->size - aligned_size - METADATA_SIZE;
+// void truncate_block(struct block_meta *block, size_t size)
+// {
+// 	size_t aligned_size = ALIGN(size);
+// 	size_t remaining_size = block->size - aligned_size - METADATA_SIZE;
 
-	struct block_meta *
-}
+// 	// VERIFICARE??
+
+// 	struct block_meta *second_block;
+// 	second_block = (struct block_meta *)((unsigned long)block + ALIGN(aligned_size + METADATA_SIZE));
+
+// 	second_block->next = block->next;
+// 	second_block->size = remaining_size;
+
+// 	block->size = aligned_size;
+// 	block->next = second_block;
+
+// 	if (block->status == STATUS_ALLOC) {
+// 		block->status = STATUS_ALLOC;
+// 		second_block->status = STATUS_FREE;
+// 	} else if (block->status == STATUS_MAPPED) {
+// 		block->status = STATUS_MAPPED;
+// 		munmap(second_block, second_block->size + METADATA_SIZE);
+// 	}
+// }
 
 void coalesce_blocks(struct block_meta *base)
 {
@@ -123,7 +140,13 @@ void coalesce_blocks(struct block_meta *base)
 	}
 }
 
+void merge_block(struct block_meta *old_block)
+{
+	old_block->size += (old_block->next)->size + METADATA_SIZE;
+	old_block->next = (old_block->next)->next;
+}
+
 struct block_meta *get_block_ptr(void *ptr)
 {
-  return (struct block_meta*) ptr - 1;
+  return (struct block_meta*)(ptr - METADATA_SIZE);
 }
