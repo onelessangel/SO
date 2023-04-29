dd4b0e9ee785491cbff6d31a34e4fa172c49dad8
onelessangel <69592470+onelessangel@users.noreply.github.com>
2023-04-29 17:42:34 +0300

    Added implementation for task and threadpool functions
    task_create
    add_task_in_queue
    get_task
    threadpool_create

--- parallel-graph/skel/os_threadpool.c
+++ parallel-graph/skel/os_threadpool.c
@@ -1,4 +1,5 @@
 #include "os_threadpool.h"
+#include "os_list.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -8,21 +9,41 @@
 /* Creates a task that thread must execute */
 os_task_t *task_create(void *arg, void (*f)(void *))
 {
-    // TODO
-    return NULL;
+    os_task_t *new_task = malloc(sizeof(*new_task));
+
+    new_task->argument = arg;
+    new_task->task = f;
+
+    return new_task;
 }
 
 /* Add a new task to threadpool task queue */
 void add_task_in_queue(os_threadpool_t *tp, os_task_t *t)
 {
-    // TODO
+    os_task_queue_t *current_node = tp->tasks;
+
+    while (current_node->next != NULL) {
+        current_node = current_node->next;
+    }
+
+    struct _node *next_node = malloc(sizeof(*next_node));
+    next_node->next = NULL;
+    next_node->task = t;
+
+    current_node->next = next_node;
 }
 
 /* Get the head of task queue from threadpool */
 os_task_t *get_task(os_threadpool_t *tp)
 {
-    // TODO
-    return NULL;
+    if (tp->tasks == NULL) {
+        return NULL;
+    }
+
+    os_task_t *target = tp->tasks->task;
+    tp->tasks = tp->tasks->next;
+
+    return target;
 }
 
 /* === THREAD POOL === */
@@ -30,8 +51,19 @@ os_task_t *get_task(os_threadpool_t *tp)
 /* Initialize the new threadpool */
 os_threadpool_t *threadpool_create(unsigned int nTasks, unsigned int nThreads)
 {
-    // TODO
-    return NULL;
+    os_threadpool_t *tp = malloc(sizeof(*tp));
+
+    tp->should_stop = 0;
+    tp->num_threads = nThreads;
+    tp->threads = malloc(nThreads * sizeof(*tp->threads));
+    tp->tasks = malloc(nTasks * sizeof(*tp->tasks));
+
+    if (pthread_mutex_init(&tp->taskLock, NULL) != 0) {
+		printf("[ERROR] Failed to init mutex\n");
+		return NULL;
+	}
+
+    return tp;
 }
 
 /* Loop function for threads */
